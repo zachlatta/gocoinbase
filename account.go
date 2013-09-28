b61c6b353adde2c965f5a6887e5645a02cefe585
Zach Latta <zach@zachlatta.com>
2013-09-28 12:16:42 -0700

    Panic on errors instead of failing silently.

--- account.go
+++ account.go
@@ -1,7 +1,6 @@
 package gocoinbase
 
 import "encoding/json"
-import "fmt"
 
 type balance struct {
 	Amount float64 `json:",string"`
@@ -17,22 +16,16 @@ type receiveAddress struct {
 func (c *Coinbase) Balance() float64 {
 	var b balance
 	err := json.Unmarshal(c.Get("/account/balance"), &b)
-	if err != nil {
-		fmt.Println(err)
-		return -1
-	}
+  panicOnError(err)
 	return b.Amount
 }
 
 func (c *Coinbase) ReceiveAddress() string {
   var r receiveAddress
   err := json.Unmarshal(c.Get("/account/receive_address"), &r)
-  if err != nil {
-    fmt.Println(err)
-    return ""
-  } else if r.Success == false {
-    fmt.Println("Getting the recieve address failed.")
-    return ""
+  panicOnError(err)
+  if r.Success == false {
+    panic("Getting the recieve address failed.")
   }
   return r.Address
 }
